122d0a81777dbc52632b511376aa67c7c75dcf99
YourmixJNR <radmakson@gmail.com>
2024-06-11 12:23:12 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import axios from 'axios';
+import { FC, useEffect, useState } from 'react';
+
+interface AppProps {
+  title: string;
 }
+
+interface Users {
+  name: {
+    first: string;
+    last: string;
+  };
+  login: {
+    uuid: string;
+  };
+  email: string;
+}
+
+const App = ({ title }: AppProps) => {
+  const [users, setUsers] = useState<Users[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const { data } = await axios.get(
+          'https://randomuser.me/api/?results=10'
+        );
+        console.log(data);
+        setUsers(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>
+        {users.map(({ login, name, email }) => {
+          return (
+            <li key={login.uuid}>
+              <div>
+                Name: {name.first} {name.last}
+              </div>
+              <div>Email: {email}</div>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
